644499047a4aba2d1d645040667f001736187151
yih-hung huang <yhh0@hotmail.com>
2018-11-15 17:57:04 -0500

    greeter change

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,7 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(String final someone) {
     return String.format("Hello, %s!", someone);
   }
 }
