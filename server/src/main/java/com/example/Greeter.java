4b2f5531158d0ddae78337d080e7b65bb717ab46
yih-hung huang <yhh0@hotmail.com>
2018-11-18 20:52:30 -0500

    fix Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  public final String greet(String final someone) {
+  public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
